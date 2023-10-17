87980b810b0e7fca617159f57d2a545bc6fda36c
passion0401lad <passion0401lad.gmail.com>
2023-10-16 19:21:58 -0700

    homepage-60%

---
+++ src/containers/index.ts
@@ -0,0 +1,4 @@
+export * from './Nutritionist';
+export * from './NotFound';
+export * from './Header';
+export * from './Footer';
---
+++ src/pages/Footer/index.tsx
@@ -0,0 +1,6 @@
+import { FooterContainer } from 'containers';
+import React from 'react';
+
+export const FooterPage: React.FC = () => {
+  return <FooterContainer />;
+};
